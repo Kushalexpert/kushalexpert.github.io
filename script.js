9d7b4d21fa371e5f3bc80538282a82d39313c4ff
Kushalexpert <89709300+Kushalexpert@users.noreply.github.com>
2021-09-05 01:40:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ $.fn.typewriter = function() {
             ">" != b.substring(a, a + 1);) a++
       }
       c.html(b.substring(d, a++) + (a & 1 ? "_" : ""));
-      a >= b.length || setTimeout(e, 5 + 50 *
+      a >= b.length || setTimeout(e, 1 + 10 *
         Math.random())
     };
     e()
